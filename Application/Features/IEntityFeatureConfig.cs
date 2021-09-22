6c426323bc1938a3e1be2caccdf3bf83cea4c1a0
Olegs <Olegs@britannica-ks.com>
2021-09-22 09:57:58 +0300

    Add resource requirements interactor

---
+++ Application/Features/IEntityFeatureConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Features
+{
+    public interface IEntityFeatureConfig
+    {
+        void ValidateFeature<T>(string feature);
+    }
+}
---
+++ Application/TrainingDevelopment/ResourceRequirements/Commands/Add/AddResourceRequirementInteractor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Features;
+
+namespace Application.TrainingDevelopment.ResourceRequirements.Commands.Add
+{
+    public class AddResourceRequirementInteractor
+    {
+        private readonly IEntityFeatureConfig entityFeatureConfig;
+
+        public AddResourceRequirementInteractor(IEntityFeatureConfig entityFeatureConfig)
+        {
+            this.entityFeatureConfig = entityFeatureConfig;
+        }
+
+        public async Task Execute<T>(
+            Guid id
+            /*ResourceRequirement requirement*/)
+        {
+            entityFeatureConfig.ValidateFeature<T>("ResourceRequirements");
+            
+            //var repository = repositoryResolver.Resolve<T>();
+            //var entity = repository.Get(id);
+
+            //entity.AddResourceRequirement(requirement)
+
+            //repository.CommitAsync();
+        }
+    }
+}
