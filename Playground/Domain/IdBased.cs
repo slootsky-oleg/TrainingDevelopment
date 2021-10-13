a30e78e0cc1d44baf94db10ecc54fe29184eb113
Olegs <Olegs@britannica-ks.com>
2021-10-13 09:31:50 +0300

    Playground

---
+++ Playground/Domain/IdBased.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playground.Domain
+{
+    public class Reference
+    {
+        public Guid Id { get; }
+        public Guid Version { get; }
+    }
+
+    public class Step
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class Task
+    {
+        public List<Step> Steps { get; set; }
+    }
+}
