5588d7150b328d41766351df1598b990ea3e6910
Olegs <Olegs@britannica-ks.com>
2021-10-13 11:07:50 +0300

    activity + generics

---
+++ Domain/Entities/TrainingDevelopment/Activities/AbstractActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities.TrainingDevelopment.Behaviour;
+using Domain.Entities.TrainingDevelopment.Behaviour.Conditions;
+using Domain.Entities.TrainingDevelopment.Behaviour.Evaluation;
+using Domain.Entities.TrainingDevelopment.Behaviour.Prerequisities;
+using Domain.Entities.TrainingDevelopment.Behaviour.ResourceRequirements;
+using Domain.Entities.TrainingDevelopment.Behaviour.Seats;
+using Domain.Entities.TrainingDevelopment.Behaviour.TargetAudience;
+using Domain.Entities.TrainingDevelopment.Tasks;
+using Domain.Entities.TrainingDevelopment.Tasks.Collections;
+using Domain.Entities.TrainingDevelopment.Tasks.Steps;
+
+namespace Domain.Entities.TrainingDevelopment.Activities
+{
+    public abstract class AbstractActivity<TTask, TStep, TResourceRequirement> : 
+        TrainingDevelopmentEntity,
+        IEvaluable,
+        IHasPrerequisites,
+        IHasConditions,
+        IHasResourceRequirements<TResourceRequirement>,
+        IHasSeats,
+        IHasTargetAudience,
+        IArchivable,
+        ITrainingCollection<TTask>
+        where TStep: Step
+        where TTask: AbstractTask<TStep, TResourceRequirement>
+        where TResourceRequirement: ResourceRequirement
+    {
+        public TimeSpan Duration { get; set; }
+        public IReadOnlyCollection<TTask> Tasks { get; }
+        public IReadOnlyCollection<AbstractTaskCollection<TTask, TStep, TResourceRequirement>> TaskCollections { get; }
+
+        public EvaluationOutline EvaluationOutline_Q { get; }
+        public PrerequisiteContainer Prerequisites_Q { get; }
+        public ResourceRequirementsContainer<TResourceRequirement> ResourceRequirements_Q { get; }
+        public SeatContainer Seats_Q { get; }
+        public TargetAudienceContainer TargetAudience_Q { get; }
+        public ExecutionConditionContainer Conditions_Q { get; }
+
+
+        public void Activate_Q()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deprecate_Q()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Obsolete_Q()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Archive_Q()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<TTask> GetTrainingItems_Q()
+        {
+            var tasksFromCollections = TaskCollections
+                .Select(s => s.GetTrainingItems_Q())
+                .SelectMany(s => s);
+
+            return tasksFromCollections
+                .Concat(Tasks)
+                .ToList();
+        }
+    }
+}
--- Domain/Entities/TrainingDevelopment/Activities/Activity.cs
+++ Domain/Entities/TrainingDevelopment/Activities/Activity.cs
@@ -1,70 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Domain.Entities.TrainingDevelopment.Behaviour;
-using Domain.Entities.TrainingDevelopment.Behaviour.Conditions;
-using Domain.Entities.TrainingDevelopment.Behaviour.Evaluation;
-using Domain.Entities.TrainingDevelopment.Behaviour.Prerequisities;
-using Domain.Entities.TrainingDevelopment.Behaviour.ResourceRequirements;
-using Domain.Entities.TrainingDevelopment.Behaviour.Seats;
-using Domain.Entities.TrainingDevelopment.Behaviour.TargetAudience;
+﻿using Domain.Entities.TrainingDevelopment.Behaviour.ResourceRequirements;
 using Domain.Entities.TrainingDevelopment.Tasks;
-using Domain.Entities.TrainingDevelopment.Tasks.Collections;
+using Domain.Entities.TrainingDevelopment.Tasks.Steps;
 
 namespace Domain.Entities.TrainingDevelopment.Activities
 {
-    public class Activity : 
-        TrainingDevelopmentEntity,
-        IEvaluable,
-        IHasPrerequisites,
-        IHasConditions,
-        IHasResourceRequirements<ResourceRequirement>,
-        IHasSeats,
-        IHasTargetAudience,
-        IArchivable,
-        ITrainingCollection<Task>
+    public class Activity : AbstractActivity<Task, Step, ResourceRequirement>
     {
-        public TimeSpan Duration { get; set; }
-        public IReadOnlyCollection<Task> Tasks { get; }
-        public IReadOnlyCollection<TaskCollection> TaskCollections { get; }
 
-        public EvaluationOutline EvaluationOutline_Q { get; }
-        public PrerequisiteContainer Prerequisites_Q { get; }
-        public ResourceRequirementsContainer<ResourceRequirement> ResourceRequirements_Q { get; }
-        public SeatContainer Seats_Q { get; }
-        public TargetAudienceContainer TargetAudience_Q { get; }
-        public ExecutionConditionContainer Conditions_Q { get; }
-
-
-        public void Activate_Q()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Deprecate_Q()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Obsolete_Q()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Archive_Q()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IReadOnlyCollection<Task> GetTrainingItems_Q()
-        {
-            var tasksFromCollections = TaskCollections
-                .Select(s => s.GetTrainingItems_Q())
-                .SelectMany(s => s);
-
-            return tasksFromCollections
-                .Concat(Tasks)
-                .ToList();
-        }
     }
 }
