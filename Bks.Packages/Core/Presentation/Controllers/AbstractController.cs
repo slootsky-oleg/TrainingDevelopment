49911b826f03d5028f6f64480d90acbefa9759c7
Olegs <Olegs@britannica-ks.com>
2021-11-24 14:06:27 +0200

    Revert "Remove folder"
    This reverts commit facc2e36a4a8b8f9b5be354bde3783e08561ab3e.

---
+++ Bks.Packages/Core/Presentation/Controllers/AbstractController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bks.Packages.Domain.Values;
+using Bks.Packages.Domain.Values.Ids;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bks.Packages.Core.Presentation.Controllers
+{
+    [ApiController]
+    public abstract class AbstractController : ControllerBase
+    {
+        protected UserFootprint GetUserFootprint()
+        {
+            var user = User
+                .Identity
+                .Name;
+
+            var userId = UserId.Of(user);
+            var now = DateTime.UtcNow;
+            return new UserFootprint(userId, now);
+        }
+    }
+}
---
+++ Bks.Packages/Core/Presentation/Filters/Swagger/FeatureGateDocumentFilter .cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Bks.Packages.Core.Presentation.Filters.Swagger
+{
+    public class FeatureGateDocumentFilter : IDocumentFilter
+    {
+        private readonly IFeatureManager featureManager;
+
+        public FeatureGateDocumentFilter(IFeatureManager featureManager)
+        {
+            this.featureManager = featureManager;
+        }
+
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            foreach (var description in context.ApiDescriptions)
+            {
+                var filterMetaData = GetFilterMetaData(description);
+                if (filterMetaData == default)
+                {
+                    continue;
+                }
+
+                var featureGateAttribute = filterMetaData as FeatureGateAttribute;
+                var feature = featureGateAttribute
+                    .Features
+                    .Single();
+
+                var isActive = featureManager
+                    .IsEnabledAsync(feature)
+                    .Result;
+                if (isActive)
+                {
+                    continue;
+                }
+
+                var descriptionPath = $"/{description.RelativePath}";
+                var apiPath = swaggerDoc
+                    .Paths
+                    .FirstOrDefault(o => o.Key.Equals(descriptionPath));
+
+                swaggerDoc
+                    .Paths
+                    .Remove(apiPath.Key);
+            }
+        }
+
+        private static IFilterMetadata GetFilterMetaData(ApiDescription apiDescription)
+        {
+            var filterPipeline = apiDescription
+                .ActionDescriptor
+                .FilterDescriptors;
+
+            var filterMetaData = filterPipeline
+                .Select(filterInfo => filterInfo.Filter)
+                .SingleOrDefault(filter => filter is FeatureGateAttribute);
+            return filterMetaData;
+        }
+    }
+}
