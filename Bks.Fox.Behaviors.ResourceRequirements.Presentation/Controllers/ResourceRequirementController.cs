c5548104bd0802ab05279e53cdb0180b5043ddad
Oleg <slootsky@gmail.com>
2021-11-25 14:09:07 +0200

    Generic RR Controller (#14)
    Co-authored-by: Olegs <Olegs@britannica-ks.com>

---
+++ Bks.Fox.Behaviors.ResourceRequirements.Presentation/Controllers/ResourceRequirementController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Bks.Fox.Behaviors.ResourceRequirements.Application.Commands.Add;
+using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.Common;
+using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.Get;
+using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.GetAll;
+using Bks.Fox.TrainingDevelopment.Domain.Entities;
+using Bks.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bks.Fox.Behaviors.ResourceRequirements.Presentation.Controllers
+{
+    public abstract class ResourceRequirementController<T> : AbstractController
+    where T: TrainingEntity
+    {
+        [HttpPost]
+        public async Task<AddResourceRequirementResponse> Add(
+            [FromServices] AddResourceRequirementInteractor<T> interactor,
+            [FromRoute] [Required] Guid id,
+            AddResourceRequirementRequest request)
+        {
+            var footprint = GetUserFootprint();
+            return await interactor.Execute(footprint, id, request);
+        }
+
+        [HttpGet]
+        public async Task<IList<GetResourceRequirementResponse>> GetAll(
+            [FromServices] GetAllResourceRequirementsInteractor<T> interactor,
+            [FromRoute] [Required] Guid id)
+        {
+            return await interactor.Execute(id);
+        }
+
+        [HttpGet("{requirementId:guid}")]
+        public async Task<GetResourceRequirementResponse> Get(
+            [FromServices] GetResourceRequirementInteractor<T> interactor,
+            [FromRoute] Guid id,
+            [FromRoute] Guid requirementId)
+        {
+            return await interactor.Execute(id, requirementId);
+        }
+    }
+}
--- Presentation.Web/Controllers/Tasks/TaskResourceRequirementController.cs
+++ Presentation.Web/Controllers/Tasks/TaskResourceRequirementController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Bks.Fox.Behaviors.ResourceRequirements.Application.Commands.Add;
 using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.Common;
 using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.Get;
 using Bks.Fox.Behaviors.ResourceRequirements.Application.Queries.GetAll;
+using Bks.Fox.Behaviors.ResourceRequirements.Presentation.Controllers;
 using Bks.Fox.TrainingDevelopment.Tasks.Domain.Entities;
 using Bks.Fox.TrainingDevelopment.Tasks.Presentation.Web.Features;
 using Bks.WebApi.Controllers;
@@ -15,35 +15,35 @@ using Microsoft.FeatureManagement.Mvc;
 namespace Bks.Fox.TrainingDevelopment.Tasks.Presentation.Web.Controllers.Tasks
 {
     [ApiController]
-    [Route("training-development/tasks/{taskId:guid}/resource-requirements")]
+    [Route("training-development/tasks/{id:guid}/resource-requirements")]
     [FeatureGate(FeatureFlag.Task)]
-    public class TaskResourceRequirementController : AbstractController
+    public class TaskResourceRequirementController : ResourceRequirementController<TrainingTask>
     {
-        [HttpPost]
-        public async Task<AddResourceRequirementResponse> Add(
-            [FromServices] AddResourceRequirementInteractor<TrainingTask> interactor,
-            [FromRoute] [Required] Guid taskId,
-            AddResourceRequirementRequest request)
-        {
-            var footprint = GetUserFootprint();
-            return await interactor.Execute(footprint, taskId, request);
-        }
-
-        [HttpGet]
-        public async Task<IList<GetResourceRequirementResponse>> GetAll(
-            [FromServices] GetAllResourceRequirementsInteractor<TrainingTask> interactor,
-            [FromRoute] [Required] Guid taskId)
-        {
-            return await interactor.Execute(taskId);
-        }
-
-        [HttpGet("{requirement:guid}")]
-        public async Task<GetResourceRequirementResponse> Get(
-            [FromServices] GetResourceRequirementInteractor<TrainingTask> interactor,
-            [FromRoute] Guid taskId,
-            [FromRoute] Guid requirementId)
-        {
-            return await interactor.Execute(taskId, requirementId);
-        }
+        // [HttpPost]
+        // public async Task<AddResourceRequirementResponse> Add(
+        //     [FromServices] AddResourceRequirementInteractor<TrainingTask> interactor,
+        //     [FromRoute] [Required] Guid taskId,
+        //     AddResourceRequirementRequest request)
+        // {
+        //     var footprint = GetUserFootprint();
+        //     return await interactor.Execute(footprint, taskId, request);
+        // }
+        //
+        // [HttpGet]
+        // public async Task<IList<GetResourceRequirementResponse>> GetAll(
+        //     [FromServices] GetAllResourceRequirementsInteractor<TrainingTask> interactor,
+        //     [FromRoute] [Required] Guid taskId)
+        // {
+        //     return await interactor.Execute(taskId);
+        // }
+        //
+        // [HttpGet("{requirement:guid}")]
+        // public async Task<GetResourceRequirementResponse> Get(
+        //     [FromServices] GetResourceRequirementInteractor<TrainingTask> interactor,
+        //     [FromRoute] Guid taskId,
+        //     [FromRoute] Guid requirementId)
+        // {
+        //     return await interactor.Execute(taskId, requirementId);
+        // }
     }
 }
