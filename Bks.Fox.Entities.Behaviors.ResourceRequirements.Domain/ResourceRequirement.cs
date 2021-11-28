6848102a730a1d8fb6a63c6908cbb4333142e679
Olegs <Olegs@britannica-ks.com>
2021-11-28 08:18:45 +0200

    Behaviour item

--- Bks.Fox.Entities.Behaviors.ResourceRequirements.Domain/ResourceRequirement.cs
+++ Bks.Fox.Entities.Behaviors.ResourceRequirements.Domain/ResourceRequirement.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Channels;
 using Bks.Fox.Behaviors.Domain;
 using Bks.Fox.Behaviors.ResourceRequirements.Domain.Criteria;
-using Bks.Fox.Domain.Entities;
-using Bks.Fox.Domain.Notifications.Changes;
 using Bks.Fox.Domain.Values;
 using Bks.Fox.Domain.Values.Ids;
 
 namespace Bks.Fox.Behaviors.ResourceRequirements.Domain
 {
-    public class ResourceRequirement : Entity, INotifyEntityChanged
+    public class ResourceRequirement : BehaviourEntity
     {
         private readonly BehaviourContainer<ResourceRequirementCriterion> criteria;
-
-        public event EventHandler<ChangeEventArgs> Changed;
 
         public Name Name { get; private set; }
         public Description Description { get; private set; }
@@ -24,11 +19,9 @@ namespace Bks.Fox.Behaviors.ResourceRequirements.Domain
 
         public ResourceRequirement(Name name)
         {
-            Changed = delegate { };
-
             Name = name;
             criteria = new BehaviourContainer<ResourceRequirementCriterion>();
-            criteria.Changed += ItemChangeHandler;
+            criteria.Changed += ChangeHandler;
         }
 
         public void SetName(UserFootprint footprint, Name name)
@@ -39,17 +32,6 @@ namespace Bks.Fox.Behaviors.ResourceRequirements.Domain
         public void SetDescription(UserFootprint footprint, Description description)
         {
             Notify(footprint, () => Description = description);
-        }
-
-        private void Notify(UserFootprint footprint, Action action)
-        {
-            action.Invoke();
-            Changed?.Notify(this, footprint);
-        }
-
-        private void ItemChangeHandler(object sender, ChangeEventArgs @event)
-        {
-            Changed?.Invoke(sender, @event);
         }
 
         //1 laptop per 2 participants
---
+++ Bks.WebApi.Cqrs/BehaviourEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Bks.Fox.Domain.Entities;
+using Bks.Fox.Domain.Notifications.Changes;
+using Bks.Fox.Domain.Values;
+
+namespace Bks.Fox.Behaviors.Domain
+{
+    public abstract class BehaviourEntity : Entity, INotifyEntityChanged
+    {
+        public event EventHandler<ChangeEventArgs> Changed;
+
+        protected void Notify(UserFootprint footprint, Action action)
+        {
+            action.Invoke();
+            Changed?.Notify(this, footprint);
+        }
+        
+        protected void Notify(object sender, ChangeEventArgs @event)
+        {
+            Changed?.Invoke(this, @event);
+        }
+
+        protected void ChangeHandler(object sender, ChangeEventArgs @event)
+        {
+            Changed?.Invoke(sender, @event);
+        }
+    }
+}
